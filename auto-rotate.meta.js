dfe7e6c7c9186c21a8dcd33020518957fdc9ae02
Ayam Penyet <88007539+ImPeekaboo@users.noreply.github.com>
2024-12-26 12:09:34 +0800

    Create auto-rotate.meta.js

---
+++ auto-rotate.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Auto Orientation Fullscreen
+// @namespace    https://github.com/ImPeekaboo
+// @version      1.0
+// @author       Ayam Penyet
+// @description  Adjusts screen orientation to match video aspect ratio in fullscreen for android browser.
+// @homepageURL  https://github.com/ImPeekaboo/auto-rotate-fullscreen
+// @supportURL   https://github.com/ImPeekaboo/auto-rotate-fullscreen/issues
+// @icon         https://cdn-icons-png.flaticon.com/512/10023/10023275.png
+// @downloadURL  https://github.com/ImPeekaboo/auto-rotate-fullscreen/raw/main/auto-rotate.user.js
+// @updateURL    https://github.com/ImPeekaboo/auto-rotate-fullscreen/raw/main/auto-rotate.meta.js
+// @grant        none
+// @match        *://*/*
+// ==/UserScript==
